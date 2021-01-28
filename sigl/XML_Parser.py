83e39249a26a433e951921f25f5220b8e32941cb
priyanka <priyankabadva@gmail.com>
2021-01-28 01:05:11 +0000

    corrected the ID formate

--- sigl/XML_Parser.py
+++ sigl/XML_Parser.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import csv
+import re
 
 
-infile = open(r"C:\Users\u_priyanka.badva\Desktop\Perfview\PerfViewData.etl.xml","r")
+infile = open(r"PerfViewData.etl.xml")
 contents = infile.read()
 soup = BeautifulSoup(contents,'xml')
 titles = soup.find_all('Event')
@@ -15,11 +16,14 @@ for item in titles:
     event = item.get('EventName')
     
     
-    if event == 'FileIO/Create' or event == 'FileIO/Read'   or event == 'FileIO/Write' or event == 'FileIO/Delete':
+    if event == 'FileIO/Create' or event == 'FileIO/Read'   or event == 'FileIO/Write' or event == 'FileIO/Delete':        
+        time = item.get('MSec')
         pname = item.get('PName')
         filename = item.get('FileName')
-        row = (event, pname, filename)
+        filename = re.escape(filename)
+        row = (event, pname, filename, time)
         fileio.append(row)
+        #fileio = sorted(set(fileio))
         
     elif event == 'UdpIp/Send' or event == 'UdpIp/Recv' or event == 'TcpIp/Send' or event == 'TcpIp/Recv':
         saddr = item.get('saddr')
@@ -27,34 +31,45 @@ for item in titles:
         sport = item.get('sport')
         dport = item.get('dport')
         pname = item.get('PName')
-        row = (event, pname, saddr, sport, daddr, dport )
+        time = item.get('MSec')
+        row = (event, pname, saddr, sport, daddr, dport, time )
         network.append(row)
+        #network = sorted(set(network))
     
     elif event == 'Process/Start' or event == 'Process/Stop':
         cmdline = item.get('CommandLine')
+        cmdline = re.escape(cmdline)
         pname = item.get('PName')
-        row = (event, pname, cmdline )
+        process_id = item.get('ProcessID')
+        process_id = re.escape(process_id)
+        parent_id = item.get('ParentID')
+        time = item.get('MSec')
+        exe = item.get('ImageFileName')
+        row = (event, pname, cmdline, time, process_id, parent_id, exe)
         process.append(row)
-        
-
-header_network = ['Event_Name', 'Process_Name', 'Src_Addr', 'Src_Port', 'Dst_Addr', 'Dst_Port' ]
- 
+        #process = sorted(set(process))
 
 with open("FileIO.csv", 'w', newline="") as f:
-    fields  = ['Event_Name', 'Process_Name', 'File_Path']
+    fields  = ['Event_Name', 'Process_Name', 'File_Path', 'Time']
     write = csv.writer(f)
     write.writerow(fields) 
     write.writerows(fileio)
     
 
 with open("Network.csv", 'w', newline="") as f:
-    fields  = ['Event_Name', 'Process_Name', 'Src_Addr', 'Src_Port', 'Dst_Addr', 'Dst_Port' ]
+    fields  = ['Event_Name', 'Process_Name', 'Src_Addr', 'Src_Port', 'Dst_Addr', 'Dst_Port', 'Time' ]
     write = csv.writer(f)   
     write.writerow(fields) 
     write.writerows(network)
     
 with open("Process.csv", 'w', newline="") as f:        
-    fields = ['Event_Name', 'Process_Name', 'File_Path'] 
+    fields = ['Event_Name',  'Process_Name', 'File_Path', 'Time', 'Process_Id', 'Parent_Id', 'exe']
+    #fields = ['Event_Name',  'Process_Name', 'File_Path', ]
     write = csv.writer(f)   
     write.writerow(fields) 
     write.writerows(process)
+    
+
+
+    
+
