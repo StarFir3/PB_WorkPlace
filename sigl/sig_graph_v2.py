9866a5917635eea404c9b99dcfbb75625bff3643
priyanka <priyankabadva@gmail.com>
2021-01-29 13:30:09 +0000

    change node shape

--- sigl/sig_graph_v2.py
+++ sigl/sig_graph_v2.py
@@ -55,8 +55,8 @@ def task_process():
         #uniq_process_name = row.Process_Id + row.exe
         # If process_id node doesn't exists create it
         if G.has_node(row.Process_Id) or G.has_node(row.Parent_Id):
-            G.add_node(row.Process_Id, FilePath = row.File_Path, Time = row.Time )
-            G.add_node(row.Parent_Id)
+            G.add_node(row.Process_Id, FilePath = row.File_Path, Time = row.Time)
+            G.add_node(row.Parent_Id, Time = row.Time )
 
             if row.Event_Name == 'Process/Start':
                 # If Process_Start, the edge should be Parent_Id to Process_Id
@@ -95,7 +95,7 @@ def task_network():
         # If process_id node doesn't exists create it
         if G.has_node(process_id) or G.has_node(row.Dst_Addr):
             G.add_node(process_id, Time = row.Time )
-            G.add_node(row.Dst_Addr)
+            G.add_node(row.Dst_Addr, shape='diamond')
 
             if row.Event_Name == 'TcpIp/Send':
             # If TcpIp/Send, the edge should be Src_Addr to Dst_Addr
@@ -105,10 +105,7 @@ def task_network():
                 # If Process_Stop, the edge should be Process_Id to Parent_Id
                 src = row.Dst_Addr
                 dst = process_id
-            elif row.Event_Name == 'TcpIp/Connect':
-            # If Process_Stop, the edge should be Process_Id to Parent_Id
-                src = process_id
-                dst = row.Dst_Addr
+
 
             if not G.has_edge(src,dst):
                 G.add_edge(src, dst, time=row.Time, EventType=row.Event_Name, xlabel= row.Event_Name[6:]) 
@@ -117,49 +114,54 @@ def task_network():
 
 def task_fileio():
     """Start with parsing fileio"""
-    df = pd.read_csv(r'csv_files/FileileIO.csv',  header = 0)
+    df = pd.read_csv(r'csv_files/FileIO.csv')
     # Reversing the CSV
-    df = df.iloc[::-1]    
+    df = df.iloc[::-1]  
+    process_id = 0     
 
-    # Process first row to get the objects
-    row = df.iloc[0]
-    src = row["Process_Name"]
-    dst = row["File_Path"]
+    # Process other rows
+    for row in df.itertuples(index=False):
+        #print(row.Process_Name)
 
-    # If parent_id and process_id node doesn't exists create it
-    if not G.has_node(src):
-        G.add_node(src, Time = row["Time"] )
-    if not G.has_node(dst):
-        G.add_node(dst, Time = row["Time"])
-    
-    if G.has_node(src) and G.has_node(dst):
-        if row['Event_Name'] == 'FileIO/Write':
-            # If Process_Start, the edge should be Parent_Id to Process_Id
-            src = row["Process_Name"]
-            dst = row["File_Path"]
-        elif row['Event_Name'] == 'FileIO/Read':
-            # If Process_Stop, the edge should be Process_Id to Parent_Id
-            src = row["File_Path"]
-            dst = row["Process_Name"]
-        elif row['Event_Name'] == 'FileIO/Create':
-            # If Process_Stop, the edge should be Process_Id to Parent_Id
-            src = row["Process_Name"]
-            dst = row["File_Path"]
-        elif row['Event_Name'] == 'FileIO/Delete':
-            # If Process_Stop, the edge should be Process_Id to Parent_Id
-            src = row["Process_Id"]
-            dst = row["Parent_Id"]
+        #for key,value in mapping.items():
+        for key, value in mapping.items():
+            #print(key,value)
+            if value == row.Process_Name:
+                process_id = key
+            #print(process_id)
 
-        if not G.has_edge(src,dst):
-            G.add_edge(src, dst, time=row["Time"], EventType=row["Event_Name"], xlabel= row["Event_Name"][8:])
+        if process_id != 0:           
+
+            if G.has_node(process_id) or G.has_node(row.File_Path):
+                G.add_node(process_id, Time = row.Time, shape='box')
+                G.add_node(row.File_Path, shape='ellipse')
+
+                if row.Event_Name == 'FileIO/Write':
+                    # If Process_Start, the edge should be Parent_Id to Process_Id
+                    src = process_id
+                    dst = row.File_Path
+                elif row.Event_Name == 'FileIO/Read':
+                    # If Process_Stop, the edge should be Process_Id to Parent_Id
+                    src = row.File_Path
+                    dst = process_id
+                elif row.Event_Name == 'FileIO/Delete':
+                    # If Process_Stop, the edge should be Process_Id to Parent_Id
+                    src = process_id
+                    dst = row.File_Path
+                elif row.Event_Name == 'FileIO/Create':
+                    # If Process_Stop, the edge should be Process_Id to Parent_Id
+                    src = process_id
+                    dst = row.File_Path
+
+                if not G.has_edge(src,dst):
+                    G.add_edge(src, dst, time=row.Time, EventType=row.Event_Name, xlabel= row.Event_Name[7:]) 
+        
 
 
 if __name__ == "__main__":
     task_process()
     task_network()
-    #task_fileio()
+    task_fileio()
     H = nx.relabel_nodes(G, mapping, copy=True)
     nx.nx_agraph.write_dot(H, "output/hello.dot")
     nx.nx_agraph.pygraphviz_layout(H)
-    
-    
